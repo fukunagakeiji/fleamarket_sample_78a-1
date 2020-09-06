a3d89eff93851578ebe09d7a2128dc1af38d49d9
bigpowermind <dirki.hrt.710@gmail.com>
2020-09-07 04:50:01 +0900

    itemテーブルのマイグレーションファイル作成

--- db/migrate/20200905195457_create_items.rb
+++ db/migrate/20200905195457_create_items.rb
@@ -1,6 +1,17 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string :name, null: false
+      t.text :explain, null: false
+      t.string :status, null: false
+      t.string :delivery_fee, null: false
+      t.string :region, null: false
+      t.string :days, null: false
+      t.string :price, null: false
+      t.integer :user_id, null: false, foreign_key: true
+      t.integer :category_id, null: false, foreign_key: true
+      t.integer :images_id, null: false, foreign_key: true
+      t.integer :brand_id, null: false, foreign_key: true
 
       t.timestamps
     end
