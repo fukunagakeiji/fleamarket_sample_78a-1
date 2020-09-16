cef6d48f692d6f3c4a88680156dc03a3a7d42815
bigpowermind <dirki.hrt.710@gmail.com>
2020-09-17 03:45:28 +0900

    creditcardモデルにアソシエーションを記載

--- db/migrate/20200916184401_create_creditcards.rb
+++ db/migrate/20200916184401_create_creditcards.rb
@@ -1,6 +1,9 @@
 class CreateCreditcards < ActiveRecord::Migration[6.0]
   def change
     create_table :creditcards do |t|
+      t.string :card_id, null: false
+      t.string :customer_id, null: false
+      t.integer :user_id, null: false, foreign_key: true
 
       t.timestamps
     end
