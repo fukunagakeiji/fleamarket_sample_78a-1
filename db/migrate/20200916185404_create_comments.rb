2f5158b7c7e0e6ff014a0aa75c540b290dff5377
bigpowermind <dirki.hrt.710@gmail.com>
2020-09-17 03:56:12 +0900

    commentモデルにアソシエーションを記載

--- db/migrate/20200916185404_create_comments.rb
+++ db/migrate/20200916185404_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
+      t.string :text, null: false
+      t.integer :items_id, foreign_key: true
+      t.integer :user_id, foreign_key: true
 
       t.timestamps
     end
