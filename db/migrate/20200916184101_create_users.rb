f91ea87cbce677631152d24c57f820fba9e57ab9
bigpowermind <dirki.hrt.710@gmail.com>
2020-09-17 03:43:29 +0900

    userモデルにアソシエーションを記載

--- db/migrate/20200916184101_create_users.rb
+++ db/migrate/20200916184101_create_users.rb
@@ -1,6 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false, unique: true
+      t.string :password, null: false, unique: true
+      t.string :icon
+      t.text :introduction
+      t.string :family_name, null: false
+      t.string :first_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.date :birthday, null: false
 
       t.timestamps
     end
