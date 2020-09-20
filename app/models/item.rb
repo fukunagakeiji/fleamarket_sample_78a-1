6befcb48549f5bcfb3543335be10ea55ab7ea71d
bigpowermind <dirki.hrt.710@gmail.com>
2020-09-20 14:58:37 +0900

    エラー箇所を修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
-  belongs_to :user dependent: :destroy
-  has_many :images dependent: :destroy
-  belongs_to :category dependent: :destroy
-  belongs_to :brand dependent: :destroy
-  has_many :comments dependent: :destroy
+  belongs_to :user, dependent: :destroy
+  has_many :images, dependent: :destroy
+  belongs_to :category, dependent: :destroy
+  belongs_to :brand, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :items dependent: :destroy
-  has_many :contact_informations dependent: :destroy
-  has_many :creditcards dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :contact_informations, dependent: :destroy
+  has_many :creditcards, dependent: :destroy
   has_many :comments
 end
