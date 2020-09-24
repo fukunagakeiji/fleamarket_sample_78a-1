f0c5f4df4092fe809f47f8853d27e0cbb4ef9acb
bigpowermind <dirki.hrt.710@gmail.com>
2020-09-25 05:12:22 +0900

    配送先の都道府県の選択完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
   # アソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :user, dependent: :destroy
   has_many :images, dependent: :destroy
   belongs_to :category, dependent: :destroy
