08029b13110d39e91d2625638c00b04b5cea89ef
bigpowermind <dirki.hrt.710@gmail.com>
2020-10-01 04:43:20 +0900

    画像のプレビューまで表示完了

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -11,10 +11,10 @@ $(document).on('turbolinks:load', ()=> {
   }
 
   // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5];
-  // id(image-box)を取得
+  let fileIndex = [1,2,3,4];
+
+  // 複数枚画像を選択できるよう実装
   $('#image-box').on('change', '.js-file', function(e) {
-    console.log("aiueo");
     // fileIndexの先頭の数字を使ってinputを作る(.appendは要素内の末尾にタグを追加する)
     $('#image-box').append(buildFileField(fileIndex[0]));
     $('.ListingMain__entire__menu__list__field__display__content').attr('for', `item_images_attributes_${fileIndex[0]}_image`)
@@ -24,9 +24,49 @@ $(document).on('turbolinks:load', ()=> {
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
   });
 
-  // $('#image-box').on('click', '.js-remove', function() {
-  //   $(this).parent().remove();
-  //   // 画像入力欄が0個にならないようにしておく
-  //   if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
-  // });
+  // 画像の削除機能
+  $('#image-box').on('click', '.js-remove', function() {
+    // 関数の中でthisを使うと、イベントの発生元となった要素を取得することができる。
+    const aiueo = $(this).parent().remove();
+    console.log(aiueo);
+
+    // 画像入力欄が0個にならないようにしておく
+    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+  });
+
+  // 画像のプレビュー表示機能
+  $(function() {
+    // 画像ファイルプレビュー表示のイベント追加 file選択時に発火するイベントを登録
+    $('#image-box').on('change', 'input[type="file"]', function(e) {
+      // targetプロパティによりイベントが発生したDOM要素を取得
+      let file = e.target.files[0];
+      // FileReaderは選択した画像を読み込むためのオブジェクト
+      let fileReader = new FileReader();
+      // 画像ファイルのURLを取得
+      fileReader.readAsDataURL(file);
+      // 画像の読み込みが完了したらプレビュー表示
+      fileReader.onload = function(e) {
+        $('.ListingMain__entire__menu__list__field__display').append($('<img>').attr({
+          src: e.target.result,
+          width: "150px",
+          class: "preview",
+        }));
+      };
+    });
+  });
 });
+
+  // // 画像のプレビュー表示
+  // // ファイルが変わった時に起動
+  // $("#image-box").on("change", 'input[type="file"].item_images_attributes_[]_image', function(e) {
+  //   // 選択したファイルのオブジェクトを取得
+  //   var file = $(this).prop("files")[0];
+  //   // FileReaderオブジェクトは選択した画像を読み込むためのオブジェクト
+  //   var fileReader = new FileReader();
+  //   // 画像ファイルのURLを取得
+  //   fileReader.readAsDataURL(file);
+  //   // 画像の読み込みが完了したらプレビュー表示
+  //   fileReader.onload = function() {
+  //   }
+  // });
+
