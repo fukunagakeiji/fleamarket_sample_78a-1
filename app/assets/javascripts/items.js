4dc34c24d36fb50c377de89fccc4d7419699521b
bigpowermind <dirki.hrt.710@gmail.com>
2020-10-02 16:28:17 +0900

    商品画像のプレビューと削除機能の実装完了

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,12 +1,12 @@
 $(document).on('turbolinks:load', ()=> {
   // 画像用のinputを生成する関数
   const buildFileField = (index)=> {
-    const html = `<div class="js-file_group">
-                    <input class="js-file" type="file"
-                    name="items[images_attributes][${index}][image]"
-                    id="item_images_attributes_${index}_image"><br>
-                    <div class="js-remove">削除</div>
-                  </div>`;
+    const html = `<div data-index-id="${index}" class="js-file_group">
+                      <input class="js-file" type="file"
+                      name="items[images_attributes][${index}][image]"
+                      id="item_images_attributes_${index}_image"><br>
+                      <div class="js-remove">削除</div>
+                    </div>`;
     return html;
   }
 
@@ -16,6 +16,7 @@ $(document).on('turbolinks:load', ()=> {
   // 複数枚画像を選択できるよう実装
   $('#image-box').on('change', '.js-file', function(e) {
     // fileIndexの先頭の数字を使ってinputを作る(.appendは要素内の末尾にタグを追加する)
+    
     $('#image-box').append(buildFileField(fileIndex[0]));
     $('.ListingMain__entire__menu__list__field__display__content').attr('for', `item_images_attributes_${fileIndex[0]}_image`)
     // shift()メソッドは配列から最初の要素を削除して、その要素を返す。このメソッドは配列のlengthを変更する。
@@ -26,10 +27,11 @@ $(document).on('turbolinks:load', ()=> {
 
   // 画像の削除機能
   $('#image-box').on('click', '.js-remove', function() {
-    // 関数の中でthisを使うと、イベントの発生元となった要素を取得することができる。
-    const aiueo = $(this).parent().remove();
-    console.log(aiueo);
-
+    // 画像のプレビュー要素を取得
+    let target_image = $('.js-file_group').data("index-id");
+    console.log(target_image);
+    // removeメソッドでプレビュー画像を削除
+    $(this).parent().remove();
     // 画像入力欄が0個にならないようにしておく
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
   });
@@ -45,8 +47,11 @@ $(document).on('turbolinks:load', ()=> {
       // 画像ファイルのURLを取得
       fileReader.readAsDataURL(file);
       // 画像の読み込みが完了したらプレビュー表示
+      const tagert_index = $('.js-file').length - 2
+      console.log(tagert_index)
+     
       fileReader.onload = function(e) {
-        $('.ListingMain__entire__menu__list__field__display').append($('<img>').attr({
+        $(`[data-index-id = "${tagert_index}"]`).append($('<img>').attr({
           src: e.target.result,
           width: "150px",
           class: "preview",
@@ -55,18 +60,3 @@ $(document).on('turbolinks:load', ()=> {
     });
   });
 });
-
-  // // 画像のプレビュー表示
-  // // ファイルが変わった時に起動
-  // $("#image-box").on("change", 'input[type="file"].item_images_attributes_[]_image', function(e) {
-  //   // 選択したファイルのオブジェクトを取得
-  //   var file = $(this).prop("files")[0];
-  //   // FileReaderオブジェクトは選択した画像を読み込むためのオブジェクト
-  //   var fileReader = new FileReader();
-  //   // 画像ファイルのURLを取得
-  //   fileReader.readAsDataURL(file);
-  //   // 画像の読み込みが完了したらプレビュー表示
-  //   fileReader.onload = function() {
-  //   }
-  // });
-
