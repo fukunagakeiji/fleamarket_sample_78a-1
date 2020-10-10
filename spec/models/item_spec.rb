b7f00e54e675c712f17ae9ddfa1744cb351d2555
bigpowermind <dirki.hrt.710@gmail.com>
2020-10-10 13:38:09 +0900

    単体テストの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,6 @@ describe Item do
     it "is valid with a name, explain, status_id, delivery_fee, region, days, price, user_id, category_id" do
       user = create(:user)
       category = create(:category)
-      #image = create(:image)
       item = FactoryBot.build(:item, user_id: user[:id], category_id: category[:id])
       # be_validはテストの対象に対してvalid?メソッドを呼んで、それが返す値によってテストの成否を判定するマッチャ。
       expect(item).to be_valid
