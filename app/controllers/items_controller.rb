b152a269de758ae3267aca8ae8ffa00450307a00
bigpowermind <dirki.hrt.710@gmail.com>
2020-10-25 04:40:21 +0900

    itemsコントローラーにカテゴリーの情報を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update]
+  before_action :set_parents, only: [:new, :create, :edit, :update]
 
   # 商品一覧表示
   def index
@@ -61,5 +62,10 @@ class ItemsController < ApplicationController
   # image_attributesに_destroyキーを追加。fields_forから送られてくるこのキーを持った情報を頼りにrailsが子モデルの更新・削除を行う。
   def item_update_params
     params.require(:item).permit(:name, :explain, :status_id, :delivery_fee, :region, :days, :price, :seller_id, :buyer_id, :auction_id, :category_id, brands_attributes: [:name], images_attributes: [:image, :id, :_destroy]).merge(seller_id: current_user.id)
+  end
+
+  def set_parents
+  # Categoryテーブルのancestryがnil（親要素の値)
+  @parents = Category.where(ancestry: nil)
   end
 end
